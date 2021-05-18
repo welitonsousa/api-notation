c1ad99066e1ee6135c17e1ed2ae3029e2594ba41
Weliton Sousa <welitonubuntu@gmail.com>
2021-05-18 11:42:27 -0300

    configure cors

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,12 @@ import express from 'express';
 import { router } from './routes';
 import * as dotenv from 'dotenv';
 
+const cors = require('cors')
+
 dotenv.config();
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(router);
 
